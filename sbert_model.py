b470fe0cfe5191e6b158d8c586e18671029a9ada
zhangyujing <9094048+zhangyujing@users.noreply.github.com>
2021-11-29 22:55:45 -0800

    Update sbert_model.py

--- sbert_model.py
+++ sbert_model.py
@@ -3,6 +3,15 @@ import collections
 import tensorflow as tf
 
 
+def _avg_pool(input_seq, pool_size):
+  return tf.nn.pool(
+      input_seq,
+      window_shape=[pool_size],
+      strides=[pool_size],
+      pooling_type='AVG',
+      padding='VALID')
+
+
 class Classifier(tf.keras.Model):
   """Sequence similarity classifier.
 
@@ -74,6 +83,22 @@ class Classifier(tf.keras.Model):
     elif pooling == 'max':
       left_outputs = tf.reduce_max(left_sequence_output, axis=1)
       right_outputs = tf.reduce_max(right_sequence_output, axis=1)
+    elif pooling == 'mean_max':
+      pool_size = 16  # hyper parameter
+      left_outputs = _avg_pool(left_sequence_output, pool_size)
+      right_outputs = _avg_pool(right_sequence_output, pool_size)
+      left_outputs = tf.reduce_max(left_outputs, axis=1)
+      right_outputs = tf.reduce_max(right_outputs, axis=1)
+    elif pooling == 'strided_mean':
+      pool_size = 16  # hyper parameter
+      left_outputs = _avg_pool(left_sequence_output, pool_size)
+      right_outputs = _avg_pool(right_sequence_output, pool_size)
+      shape = tf.shape(left_outputs)
+      left_outputs = tf.reshape(shape, [-1, s[1] * s[2]])
+      right_outputs = tf.reshape(shape, [-1, s[1] * s[2]])
+      pool_layer = tf.keras.layers.Dense(512, activation='relu') # 512: hyper parameter
+      left_outputs = pool_layer(left_outputs)
+      right_outputs = pool_layer(right_outputs)
     else:
       raise ValueError('Pooling %s is not supported: %s' % pooling)
 
@@ -122,3 +147,75 @@ class Classifier(tf.keras.Model):
   @classmethod
   def from_config(cls, config, custom_objects=None):
     return cls(**config)
+  
+MASK_ID = 103
+
+def masked_ids(ids, mask, dropout_rate=0.05):
+  if dropout_rate == 0.0:
+    return ids
+  random_tensor = tf.random.uniform(tf.shape(ids), minval=0, maxval=1)
+  retained = tf.greater(random_tensor, dropout_rate)
+  dropped = tf.cast(tf.logical_not(retained), ids.dtype) * mask
+  retained_ids = tf.cast(retained, ids.dtype) * ids
+  retained_ids = retained_ids + dropped * MASK_ID
+  return retained_ids
+  
+  
+class SSLClassifier(tf.keras.Model):
+  """Two-tower self-supervised classifer with dynamic masking.
+
+    Args:
+      classifer: a tf.keras.Model defines a classifer network.
+      max_seq_length: maximum sequence length.
+      input_dropout_rate: dropout rate for dynamic masking.
+  """
+
+  def __init__(self, classifier, max_seq_length, input_dropout_rate=0.05, **kwargs):
+    self.max_seq_length = max_seq_length
+    self.input_dropout_rate = input_dropout_rate
+
+    left_word_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='left_word_ids')
+    left_mask = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='left_mask')
+    left_type_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='left_type_ids')
+
+    right_word_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='right_word_ids')
+    right_mask = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='right_mask')
+    right_type_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int32, name='right_type_ids')
+
+    orig_inputs = [left_word_ids, left_mask, left_type_ids,
+                   right_word_ids, right_mask, right_type_ids]
+    masked_left_ids = masked_ids(left_word_ids, left_mask, input_dropout_rate)
+    masked_right_ids = masked_ids(right_word_ids, right_mask, input_dropout_rate)
+    ssl_inputs = [masked_left_ids, left_mask, left_type_ids,
+                  masked_right_ids, right_mask, right_type_ids]
+
+    inputs = {
+        'left_input_ids': left_word_ids,
+        'left_input_mask': left_mask,
+        'left_input_type_ids': left_type_ids,
+        'right_input_ids': right_word_ids,
+        'right_input_mask': right_mask,
+        'right_input_type_ids': right_type_ids
+    }
+    prob1 = classifier(orig_inputs)
+    prob2 = classifier(ssl_inputs)
+
+    outputs = {}
+    outputs['main'] = prob1
+    outputs['ssl'] = prob2
+    super(SSLClassifier, self).__init__(inputs=inputs, outputs=outputs, **kwargs)
+    self._classifier = classifier
+
+    config_dict = {
+        'classifier': self._classifier,
+        'max_seq_length': self.max_seq_length,
+        'input_dropout_rate': self.input_dropout_rate
+    }
+    config_cls = collections.namedtuple('Config', config_dict.keys())
+    self._config = config_cls(**config_dict)
